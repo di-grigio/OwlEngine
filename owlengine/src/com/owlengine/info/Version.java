07500f93c7df2bee52cf1059567b95709aa2b97e
Leo-Di-Grigio <leo.di.grigio@gmail.com>
2016-04-19 15:59:11 +0300

    0.1.19

--- owlengine/src/com/owlengine/info/Version.java
+++ owlengine/src/com/owlengine/info/Version.java
@@ -4,9 +4,5 @@ public final class Version {
 
 	public static final int MAJOR = 0;
 	public static final int MINOR = 1;
-<<<<<<< HEAD
-	public static final int REVISION = 18;
-=======
-	public static final int REVISION = 17;
->>>>>>> origin/master
+	public static final int REVISION = 19;
 }
